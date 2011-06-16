53744d64218fcfff583043857163853b4cbe48da
Dominic Tarr <dominic.tarr@gmail.com>
2011-06-15 17:20:15 -0700

    FSM based saver is naive. rolling back

--- controllers/github.js
+++ controllers/github.js
@@ -4,6 +4,39 @@ var log = console.log
   , overwrite = require('../model/overwrite')
 
 module.exports = function (db,Repo,config) { //I think dependency injection may be an antipattern
+  function filter (obj,keys) {
+    var n = {}
+    Object.keys(obj).forEach(function (i){
+      if(!~keys.indexOf(i))
+        n[i] = obj[i]
+    })
+    return n
+  }
+
+  function save (repo) { //pull this out also.
+    if(repo._id && !repo.saving){
+      repo.saving = true
+
+      db.get('' + repo._id, function (err, doc){
+
+        var obj = filter (doc,['_events'])
+        obj._id = '' + repo._id
+        obj._rev = doc && doc._rev
+        obj.time = new Date()
+        db.save(obj,
+          function (err, data){
+            repo._rev = data._rev
+            repo.saving = null
+            if(repo.changed){
+              save(repo)
+              repo.changed = null
+            }
+          })
+      })
+    } else {
+      repo.changed = true
+    }
+  }
 
   return function (req,cont){
     var payload
@@ -34,9 +67,7 @@ module.exports = function (db,Repo,config) { //I think dependency injection may 
 
     repo.on('change',function (event){
       console.log('change',event, repo._id)
-      overwrite(db,repo,function (){
-        console.log("SAVED",event, repo._id)
-      })
+      save(repo)
     })
 
     repo.integrate(function (err,data){
@@ -44,12 +75,13 @@ module.exports = function (db,Repo,config) { //I think dependency injection may 
       if(err){//not necessarily the right error message.
         return cont({statusCode: 503, error: err, reason:  "could not connect to github or npm", data: data})
       }
-      overwrite(db,repo,function (err2,data2){   //save to database.
+      save(db,repo)
+      //,function (err2,data2){   //save to database.
         if(err)
           return cont({statusCode: 503, error: err, reason:  "database error", data: data})
   
-        cont(null, data2)
-      })
+        cont(null, {ok: true} )
+      //})
     }) 
   }
 }
